922d8e87cdeb8c57466facd5c0c7ae138f6b2e9b
hatmanstack <hatmanstack@protonmail.com>
2024-08-10 18:17:26 -0500

    Android/Web Compatibility

--- components/navigation/TabBarIcon.tsx
+++ components/navigation/TabBarIcon.tsx
@@ -1,9 +1,10 @@
 // You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
 
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
-import MaterialIconsProps from '@expo/vector-icons/build/createIconSet';
 import { type ComponentProps } from 'react';
 
-export function TabBarIcon({ style, ...rest }: MaterialIcons<ComponentProps<typeof MaterialIconsProps>['name']>) {
+type MaterialIconsProps = ComponentProps<typeof MaterialIcons>;
+
+export function TabBarIcon({ style, ...rest }: MaterialIconsProps) {
   return <MaterialIcons size={28} style={[{ marginBottom: -3 }, style]} {...rest} />;
 }
